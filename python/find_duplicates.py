21ccdec92544e1f781ad339b0c48a921562164bb
ktaylo-funky-pants <ktaylo11@unca.edu>
2025-09-09 12:23:06 -0400

    find duplicates in python

---
+++ python/find_duplicates.py
@@ -0,0 +1,24 @@
+def find_duplicates_nested_loop(l: list) -> list:
+	new_list = []
+	l.sort()
+	seen = set()
+	for i in range(len(l)-1):
+		if l[i] == l[i+1]:
+			if l[i] not in seen:
+				new_list.append(l[i])
+				seen.add(l[i])
+	return new_list
+
+# In Python, if __name__ == "__main__" is a conditional check that determines whether 
+# a Python file is being run as the main program or imported as a module into another 
+# script.
+if __name__ == "__main__":
+    sample1 = [3, 7, 5, 6, 7, 4, 8, 5, 7, 66]
+    sample2 = [3, 5, 6, 4, 4, 5, 66, 6, 7, 6]
+    sample3 = [3, 0, 5, 1, 0]
+    sample4 = [3]
+    
+    print("Sample 1:", find_duplicates_nested_loop(sample1))
+    print("Sample 2:", find_duplicates_nested_loop(sample2))
+    print("Sample 3:", find_duplicates_nested_loop(sample3))
+    print("Sample 4:", find_duplicates_nested_loop(sample4))
